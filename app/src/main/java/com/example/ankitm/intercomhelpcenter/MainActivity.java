0c78847f86ec99f2a9407f6631565e1ff69fea93
Ankit Maheswari <ankitmaheswari@Ankits-MacBook-Pro.local>
2018-12-06 20:28:19 +0530

    Add registration call before messenger is displayed

--- app/src/main/java/com/example/ankitm/intercomhelpcenter/MainActivity.java
+++ app/src/main/java/com/example/ankitm/intercomhelpcenter/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.ankitm.intercomhelpcenter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import io.intercom.android.sdk.Intercom;
 import io.intercom.android.sdk.identity.Registration;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +21,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void showHelpCenter(View view) {
         Intercom.client().displayHelpCenter();
+        Log.v(TAG, "show help center");
     }
 
     public void showMessenger(View view) {
-        Registration registration = Registration.create()
-                .withUserId("9958954682");
-        Intercom.client().registerIdentifiedUser(registration);
-
+        registerIntercomUser();
         //open intercom messenger
         Intercom.client().displayMessenger();
+        Log.v(TAG, "display messenger");
     }
     
     private void registerIntercomUser() {
         Registration registration = Registration.create()
                 .withUserId("9958954682");
         Intercom.client().registerIdentifiedUser(registration);
+        Log.v(TAG, "user registered");
     }
 }
